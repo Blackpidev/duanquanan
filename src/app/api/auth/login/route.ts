36406730bd89d513ae086e1659e688ec2e042ef6
blackpidev <xuanhanh.qn1995@gmail.com>
2024-12-11 07:01:06 +0700

    tạo refresh token route handler

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -5,7 +5,6 @@ import jwt from "jsonwebtoken";
 import { HttpError } from "@/lib/http";
 
 export async function POST(request: Request) {
-    console.log(request);
   const res = (await request.json()) as LoginBodyType;
   const cookieStore = cookies();
   try {
---
+++ src/app/api/auth/refresh-token/route.ts
@@ -0,0 +1,53 @@
+import authapiRequest from "@/app/apiRequests/auth";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+
+export async function POST(request: Request) {
+  const cookieStore = cookies();
+  const refreshToken = cookieStore.get("accessToken")?.value;
+  if (!refreshToken) {
+    return Response.json(
+      {
+        message: "Không tìm thất refreshToken",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  try {
+    const { payload } = await authapiRequest.sRefreshToken({
+      refreshToken,
+    });
+    const decodeAccessToken = jwt.decode(payload.data.accessToken) as {
+      exp: number;
+    };
+    const decodeRefreshToken = jwt.decode(payload.data.refreshToken) as {
+      exp: number;
+    };
+    cookieStore.set("accessToken", payload.data.accessToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodeAccessToken.exp * 1000,
+    });
+    cookieStore.set("refreshToken", payload.data.refreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodeRefreshToken.exp * 1000,
+    });
+    return Response.json(payload);
+  } catch (error: any) {
+    return Response.json(
+      {
+        message: error.message ?? "Có lỗi xảy ra",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+}
