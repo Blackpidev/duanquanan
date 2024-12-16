49aa263fe7438c04937d118c70977604fb8ff958
blackpidev <xuanhanh.qn1995@gmail.com>
2024-12-16 14:31:27 +0700

    Hiển thị danh sách account

--- src/app/manage/accounts/account-table.tsx
+++ src/app/manage/accounts/account-table.tsx
@@ -43,6 +43,7 @@ import {
 } from '@/components/ui/alert-dialog'
 import { useSearchParams } from 'next/navigation'
 import AutoPagination from '@/components/auto-pagination'
+import { useGetAccountList } from '@/app/queries/useAccount'
 
 type AccountItem = AccountListResType['data'][0]
 
@@ -59,6 +60,15 @@ const AccountTableContext = createContext<{
 })
 
 export const columns: ColumnDef<AccountType>[] = [
+  {
+    id: 'stt', header: 'STT', cell: ({
+      row
+    })=>{
+    return (
+      <>{row.index+1}</>
+    )
+  }
+  },
   {
     accessorKey: 'id',
     header: 'ID'
@@ -165,7 +175,8 @@ export default function AccountTable() {
   // const params = Object.fromEntries(searchParam.entries())
   const [employeeIdEdit, setEmployeeIdEdit] = useState<number | undefined>()
   const [employeeDelete, setEmployeeDelete] = useState<AccountItem | null>(null)
-  const data: any[] = []
+  const accountListQuery = useGetAccountList()
+  const data = accountListQuery.data?.payload.data ?? []
   const [sorting, setSorting] = useState<SortingState>([])
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
