726b984592367a4cb9be99269d60082d182fc780
blackpidev <xuanhanh.qn1995@gmail.com>
2024-12-20 07:21:06 +0700

    Thêm thông tin vào QR Code canvas

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -15,18 +15,42 @@ export default function QRcodeTable({
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
+    const canvas = canvasRef.current!;
+    canvas.width = width
+    canvas.height = width + 70;
+    const canvasContext = canvas.getContext("2d")!;
+    canvasContext.fillStyle = "#fff";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    canvasContext.font = "16px Arial";
+    canvasContext.textAlign = "center";
+    canvasContext.fillStyle = "#000";
+    canvasContext.fillText(
+      `Bàn số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 20
+    );
+    canvasContext.fillText(
+      `Quét mã QR để gọi món`,
+      canvas.width / 2,
+      canvas.width + 50
+    );
+    const virtalCanvas = document.createElement("canvas");
+
     QRCode.toCanvas(
-      canvas,
+      virtalCanvas,
       getTableLink({
         token,
         tableNumber,
       }),
+      {
+        width: width,
+        margin: 4,
+      },
       function (error) {
         if (error) console.error(error);
-        console.log("success!");
+        canvasContext.drawImage(virtalCanvas, 0, 0, width, width);
       }
     );
-  }, [token, tableNumber]);
+  }, [token, tableNumber, width]);
   return <canvas ref={canvasRef} />;
 }
